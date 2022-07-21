d9075d0d8ca701e37becfc80d3e9ebd7feadfdbe
Alexandr Kopach <Akavadakadon@gmail.com>
2022-07-21 22:16:02 +0300

    temp

--- Proj1/ObjectLifeCycle.h
+++ Proj1/ObjectLifeCycle.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Rational.h"
+#include "SearchServer.h"
 
 Rational Add(Rational r1, Rational r2) {
     int numerator = r1.Numerator() * r2.Denominator() + r2.Numerator() * r1.Denominator();
@@ -11,5 +12,15 @@ Rational Add(Rational r1, Rational r2) {
 
 void ObjectLifeCycle()
 {
+    cout << "\n\n ObjectLifeCycle \n\n";
+    cout << "\n\n Rational \n\n";
 	Rational r(6,2);
+    cout << "\n\n Initialization SearchServer \n\n";
+    set<string> stop_words_set = { "и"s, "в"s, "на"s };
+    auto asdsa = { "и"s, "в"s, "на"s };
+    vector<string> stop_words_vector = { "и"s, "в"s, "на"s, ""s, "в"s };
+    sprint3::SearchServer search_server2(stop_words_vector);
+    sprint3::SearchServer search_server3("  и  в на   "s);
+    sprint3::SearchServer ss;
+
 }
---
+++ Proj1/SearchServer.h
@@ -0,0 +1,79 @@
+﻿#pragma once
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <execution>
+#include <cmath>
+
+
+using namespace std;
+
+namespace sprint3 {
+#ifndef MAX_RESULT_DOCUMENT_COUNT
+#define MAX_RESULT_DOCUMENT_COUNT 5
+#endif
+
+    struct Document {
+        int id;
+        double relevance;
+        int rating;
+    };
+    enum class DocumentStatus {
+        ACTUAL,
+        IRRELEVANT,
+        BANNED,
+        REMOVED,
+    };
+    struct Query {
+        set<string> plus_words;
+        set<string> minus_words;
+    };
+    struct QueryWord {
+        string data;
+        bool is_minus;
+        bool is_stop;
+    };
+    struct DocumentData {
+        int rating;
+        DocumentStatus status;
+    };
+
+    class SearchServer {
+    public:
+        void SetStopWords(const string& text);
+        void AddDocument(int document_id, const string& document, DocumentStatus status, const vector<int>& ratings);
+        template <typename DocumentPredicate> vector<Document> FindTopDocuments(const string& raw_query, DocumentPredicate document_predicate) const;
+        vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status) const;
+        vector<Document> FindTopDocuments(const string& raw_query) const;
+        int GetDocumentCount() const;
+        tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query, int document_id) const;
+
+        explicit SearchServer() = default;
+        explicit SearchServer(const string stop_words);
+        template <typename StringCollection> 
+        explicit SearchServer(const StringCollection& stop_words_container)
+        {
+            for (string str : stop_words_container)
+                stop_words_.insert(str);
+        }
+    private:
+        set<string> stop_words_;
+        map<int, DocumentData> documents_;
+        map<string, map<int, double>> word_to_document_freqs_;
+
+        vector<string> SplitIntoWords(const string& text) const;
+        bool IsStopWord(const string& word) const;
+        vector<string> SplitIntoWordsNoStop(const string& text) const;
+        QueryWord ParseQueryWord(string text) const;
+        Query ParseQuery(const string& text) const;
+        double ComputeWordInverseDocumentFreq(const string& word) const;
+        template <typename DocumentPredicate> vector<Document> FindAllDocuments(const Query& query, DocumentPredicate document_predicate) const;
+
+        static int ComputeAverageRating(const vector<int>& ratings);
+    };
+
+}
+
